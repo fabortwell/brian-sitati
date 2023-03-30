0b39620b18abe02c74babb1d6e1a0d26f4c842ed
user <tre@Brian.local>
2023-03-30 17:12:37 +0300

    fixed hero bug displaying computer image in 3D

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -11,8 +11,36 @@ const Computers = () => {
    <mesh>
     <hemisphereLight intensity={0.15} 
     groundColor='black' />
+    <pointLight intensity={1} />
+    <primitive 
+    object={computer.scene}
+    scale={0.75}
+    position={[0, -3.25, -1.5]}
+    />
    </mesh>
   )
 }
+const ComputersCanvas = () => {
 
-export default Computers
+  return (
+    <Canvas
+      frameLoop='demand'
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers />
+      </Suspense>
+      
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default ComputersCanvas;
