36cb973f924f087de2d20b2b13fef7ae5d4ea962
user <tre@Brian.local>
2023-04-03 18:31:59 +0300

    completed working on the about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import Tilt from 'react-tilt';
 import { motion } from 'framer-motion';
 import { styles } from '../styles';
 import { services } from '../constants';
-
+import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from '../utils/motion'
 
 
@@ -66,4 +66,4 @@ const About = () => {
   )
 }
 
-export default About
+export default SectionWrapper(About, "about");
